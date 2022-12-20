2990fbdf39e4068ac590b01221a79eb74e590f2a
matheus <matheuslima20111997@gmail.com>
2022-12-20 13:19:36 -0300

    fix bug

--- src/service.spec.ts
+++ src/service.spec.ts
@@ -1,10 +1,10 @@
 import { gerarArrayNumeros } from "./service";
 
-test('Test generate numbers service', async () => {
+describe('Test generate numbers service', () => {
 
-    it('should generate 5 random numbers'), async () => {
+    it('should generate 5 random numbers', async () => {
         const arr = await gerarArrayNumeros(5)
         expect(arr.length).toBe(5);
-    }
+        
+    })
 });
-
