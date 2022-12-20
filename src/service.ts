65ddbbc1d60f8b6cdc551cf76587a025fa7ebb72
matheus <matheuslima20111997@gmail.com>
2022-12-20 13:20:50 -0300

    criando erro

--- src/service.ts
+++ src/service.ts
@@ -1,6 +1,6 @@
 export async function gerarArrayNumeros(quantidade: number) {
     const result = []
-    for (let i = 0; i < quantidade; i++) {
+    for (let i = 0; i < 2; i++) {
         result.push(Math.floor(Math.random() * 10))
     }
     return result
