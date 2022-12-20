c6d22e0f1ba681a26d4b31a635cb3cda15dc9358
matheus <matheuslima20111997@gmail.com>
2022-12-20 13:16:40 -0300

    melhorando testes

--- src/service.spec.ts
+++ src/service.spec.ts
@@ -1,7 +1,10 @@
 import { gerarArrayNumeros } from "./service";
 
 test('Test generate numbers service', async () => {
-    const arr = await gerarArrayNumeros(5)
-    expect(arr.length).toBe(5);
+
+    it('should generate 5 random numbers'), async () => {
+        const arr = await gerarArrayNumeros(5)
+        expect(arr.length).toBe(5);
+    }
 });
 
